18220ec311786bdd9a389318ab0d486a2d147401
pennyweng <penny_weng@htc.com>
2019-03-05 22:35:47 +0800

    Updates

---
+++ precache-manifest.cc72047b2ca46d5161e9bf9a4e0567e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f7caab5b482cd25c381",
+    "url": "/material-sense/static/css/main.4b1c13dc.chunk.css"
+  },
+  {
+    "revision": "3f7caab5b482cd25c381",
+    "url": "/material-sense/static/js/main.3f7caab5.chunk.js"
+  },
+  {
+    "revision": "9f6f45c1b23bc1097a5b",
+    "url": "/material-sense/static/js/1.9f6f45c1.chunk.js"
+  },
+  {
+    "revision": "09f4e86c218a577ad5da",
+    "url": "/material-sense/static/js/runtime~main.09f4e86c.js"
+  },
+  {
+    "revision": "208f3d38b50b2147d8c20291babed81a",
+    "url": "/material-sense/static/media/shape.208f3d38.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/material-sense/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f6b6b51b1735d2ecde04f949b081ee3c",
+    "url": "/material-sense/index.html"
+  }
+];
